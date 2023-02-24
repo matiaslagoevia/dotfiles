e5619357631cbcf479e591fc24909da2ba99373c
Matias Lago Evia <matiaslagoevia@gmail.com>
2023-02-24 14:59:27 -0500

    fix telescope mappings

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -3,11 +3,12 @@ local builtin = require('telescope.builtin')
 -- Below, "project" refers to the pwd where neovim was launched
 -- It may not reflect the open buffer's directory
 
--- Find in all project files
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
--- Find in all git-tracked project files
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- Find in all project files (find files)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- Find in all git-tracked project files (project files)
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 -- Perform (rip)grep search on all project files
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("(rip)grep > ") })
 end)
+
